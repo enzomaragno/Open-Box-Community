d4734d0f94722d7c1804d7271d801dfd9742c748
Maragno <79205858+enzomaragno@users.noreply.github.com>
2022-04-22 01:27:07 -0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,20 +2,20 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "say",
-  //o bot fala sua msg!
+
 
 
   
 
   run: async(client, message, args) => {
-    let msg = args.join(" "); //setando....
+    let msg = args.join(" "); 
 
 
     if (!message.member.permissions.has("MANAGE_GUILD")) return message.reply(`${message.author} **Voce não possui permissão para esse comando.**`);  
     
-    if (!msg) return message.channel.send(`❌ | ${message.author} Você precisa digitar uma mensagem`); //verificando se há alguma mensagem
+    if (!msg) return message.channel.send(`❌ | ${message.author} Você precisa digitar uma mensagem`); 
 
-    message.channel.send(`${msg}`) //comando para o bot falar sua mensagem
+    message.channel.send(`${msg}`) 
     message.delete().catch(O_o => {});
 
   }
