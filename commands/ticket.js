181d65d51dc43fc8a08c5af644f701cd83aa72bb
Maragno <79205858+enzomaragno@users.noreply.github.com>
2022-05-12 21:20:14 -0300

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -3,44 +3,22 @@ const Discord = require("discord.js")
 module.exports = {
     name: "t", 
     aliases: ["ticket"], 
-    //creditos para Maragnoo#9131
-//creditos para Maragnoo#9131
-//creditos para Maragnoo#9131
+    
 
 
     run: async(client, message, args) => {
 
-        if (!message.member.permissions.has("ADMINISTRATOR")) {
-            message.reply(`Você não possui permissão para utilizar este comando.`)
-        } else {
-            let embed = new Discord.MessageEmbed()
-            .setColor("#2F3136")
-            .setAuthor(`Atendimento`)
-            .setFooter(`Open BOX`)
-            .setTimestamp(new Date())
-            .setThumbnail("https://cdn.discordapp.com/icons/827725704711110659/8e77c3930447c91937669ff09411b389.png?size=2048")
-            .addField(`Atenção!`, `<a:Seta:820394451167608922> Abra um ticket clicando no botão abaixo. \`🎫\`\n<:information:946890994375327815> Não abra um TICKET sem ter algo relevante. Abrir apenas por abrir irá gerar punições.`);
-
-            let abrir = new Discord.MessageActionRow()
-            .addComponents(
-                new Discord.MessageButton()
-                .setCustomId("t")
-                .setEmoji("🎫")
-                .setLabel("Abrir Ticket")
-                .setStyle("PRIMARY")
-            );
-
-            message.channel.send({ embeds: [embed], components: [abrir] }).then( () => {
-                message.delete()
-            });
-        }
+      let privado = new Discord.MessageEmbed()
+      .setTitle("Esse script pode ser adquirido em nosso servidor")
+      .setDescription("Abra um ticket para maiores informações")
+      message.channel.send({embeds: [privado]})
+        
+        
+        
+      
 
        
-        //creditos para Maragnoo#9131
-//creditos para Maragnoo#9131
-//creditos para Maragnoo#9131
-//creditos para Maragnoo#9131
-//creditos para Maragnoo#9131
+      
 
     }
 }
